54492ee96ceb619d7be7afe245ef2474d1e9d41a
Bain Gurley <baingurley@gmail.com>
2026-07-29 16:06:29 -0500

    chore: bump to 1.0.4 (72)

--- Rivulet/Views/Components/WhatsNewView.swift
+++ Rivulet/Views/Components/WhatsNewView.swift
@@ -140,7 +140,7 @@ struct WhatsNewView: View {
     // MARK: - Changelog Data
 
     static let changelogs: [(version: String, features: [String])] = [
-        ("1.0.4 (72)", [ // confirm build number at release
+        ("1.0.4 (72)", [
             "Updated AetherEngine to 6.0.2",
             "New Instant Resume setting in Playback, on by default. Turn it off and Continue Watching tiles open the preview like every other Home row instead of playing right away",
         ]),
